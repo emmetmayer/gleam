b8a9548bfd3857f30b1995ae784d2d6be4bfb93e
emmetmayer <emmetcmayer@gmail.com>
2022-09-16 11:46:00 -0400

    Update heap.c
    changes

--- src/heap.c
+++ src/heap.c
@@ -15,11 +15,20 @@ typedef struct arena_t
 	struct arena_t* next;
 } arena_t;
 
+typedef struct backtrace_t
+{
+	void* address;
+	void** trace;
+	struct backtrace_t* next;
+	size_t size;
+} backtrace_t;
+
 typedef struct heap_t
 {
 	tlsf_t tlsf;
 	size_t grow_increment;
 	arena_t* arena;
+	backtrace_t* backtrace;
 } heap_t;
 
 heap_t* heap_create(size_t grow_increment)
@@ -37,13 +46,14 @@ heap_t* heap_create(size_t grow_increment)
 	heap->grow_increment = grow_increment;
 	heap->tlsf = tlsf_create(heap + 1);
 	heap->arena = NULL;
+	heap->backtrace = NULL;
 
 	return heap;
 }
 
 void* heap_alloc(heap_t* heap, size_t size, size_t alignment)
 {
-	void* address = tlsf_memalign(heap->tlsf, alignment, size);
+	void* address = tlsf_memalign(heap->tlsf, alignment, size + sizeof(backtrace_t));
 	if (!address)
 	{
 		size_t arena_size =
@@ -65,14 +75,41 @@ void* heap_alloc(heap_t* heap, size_t size, size_t alignment)
 		arena->next = heap->arena;
 		heap->arena = arena;
 
-		address = tlsf_memalign(heap->tlsf, alignment, size);
+		address = tlsf_memalign(heap->tlsf, alignment, size + sizeof(backtrace_t));
 	}
+
+	if (address)
+	{
+		backtrace_t* backtrace = (backtrace_t*) ((int*) address + size/sizeof(int));
+		backtrace->address = address;
+		backtrace->trace = (int**)((int*)backtrace + sizeof(int*)/sizeof(int));
+		debug_backtrace(backtrace->trace, 10);
+		backtrace->next = heap->backtrace;
+		heap->backtrace = backtrace;
+	}
+	
+
 	return address;
 }
 
 void heap_free(heap_t* heap, void* address)
 {
 	tlsf_free(heap->tlsf, address);
+	backtrace_t* trace = heap->backtrace;
+	if (trace->address == address)
+	{
+		heap->backtrace = trace->next;
+	}
+	else
+	{
+		while (trace->next->address != address)
+		{
+			trace = trace->next;
+		}
+		trace->next = trace->next->next;
+	}
+	
+	
 }
 
 void heap_destroy(heap_t* heap)
@@ -86,6 +123,11 @@ void heap_destroy(heap_t* heap)
 		VirtualFree(arena, 0, MEM_RELEASE);
 		arena = next;
 	}
-
+	backtrace_t* trace = heap->backtrace;
+	while (trace)
+	{
+		debug_print(k_print_warning, "leaked");
+		trace = trace->next;
+	}
 	VirtualFree(heap, 0, MEM_RELEASE);
 }
